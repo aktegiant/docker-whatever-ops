eb44390097ed47a3ef1af13185c96e3f9f6d13a1
aktegiant <ahkarshwebaw@gmail.com>
2024-06-07 10:52:11 +0700

    "Update index.html title and content, remove MongoDB and Redis connection checks, and simplify logging configuration."

--- src/index.mjs
+++ src/index.mjs
@@ -8,32 +8,6 @@ import path from "path";
 const app = express();
 const port = process.env.PORT;
 
-
-async function checkMongoConnection() {
-  try {
-    await mongoose.connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`);
-    console.log("Connected to MongoDB successfully!");
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    process.exit(1); // Exit the application on connection failure
-  }
-}
-
-async function checkRedisConnection() {
-  try {
-    const client = redis.createClient({
-      url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    });
-    await client.connect();
-    console.log("Connected to Redis successfully!");
-  } catch (err) {
-    console.error("Error connecting to Redis:", err);
-    process.exit(1); // Exit the application on connection failure
-  }
-}
-
-const logDir = path.resolve(process.cwd(), "logs");
-
 // Configure express-winston to log to a file
 app.use(
   expressWinston.logger({
@@ -42,14 +16,7 @@ app.use(
         json: true,
         colorize: true,
       }),
-      new winston.transports.File({
-        filename: path.join(logDir, "log.txt"),
-      }),
     ],
-    format: winston.format.combine(
-      winston.format.colorize(),
-      winston.format.json()
-    ),
   })
 );
 
@@ -59,10 +26,6 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
-(async () => {
-  await Promise.all([checkMongoConnection(), checkRedisConnection()]);
-
-  app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-  });
-})();
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
